ffe0380c39022f5a1357428be1ceae097a4a2be3
Chris Patterson <chris@phatboyg.com>
2013-01-23 08:04:44 -0800

    Updated to MT 2.7.1

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A MassTransit Routing Slip pattern implementation")]
 [assembly: AssemblyProduct("MassTransit.Courier")]
 [assembly: AssemblyCopyright("Copyright 2013 Chris Patterson")]
-[assembly: AssemblyVersion("2.7.0")]
-[assembly: AssemblyFileVersion("2.7.0")]
+[assembly: AssemblyVersion("2.7.1")]
+[assembly: AssemblyFileVersion("2.7.1")]
 
-[assembly: AssemblyInformationalVersion("2.7.0.c26906")]
+[assembly: AssemblyInformationalVersion("2.7.1.af1245")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(true)]
 
